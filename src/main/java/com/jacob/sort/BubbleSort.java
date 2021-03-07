2f4e7f478f65e5f9dd2b3de35cdbb9a4be2352e2
Jacob <978142348@qq.com>
2021-03-07 18:10:28 +0800

    选择排序

--- src/main/java/com/jacob/sort/BubbleSort.java
+++ src/main/java/com/jacob/sort/BubbleSort.java
@@ -3,10 +3,18 @@ package com.jacob.sort;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] numbs = {2,8,5,6,1,4};
+//        int[] numbs = {2,8,5,6,1,4};
+
+        int[] numbs = new int[80000];
+
+        for (int i = 0; i < 80000; i++) {
+            numbs[i] = (int) (Math.random() * 10000);
+        }
 
         int m = numbs.length;
         int flag = 1;
+
+        long start = System.currentTimeMillis();
 
         while (m > 0 && flag == 1){
             flag = 0;
@@ -19,13 +27,19 @@ public class BubbleSort {
                     numbs[i + 1] = numbs[i];
                     numbs[i] = temp;
                 }
+//                for (int numb : numbs) {
+//                    System.out.print(numb + "  ");
+//                }
+//                System.out.println();
             }
+//            System.out.println("----------------");
             m--;
         }
 
-        for (int numb : numbs) {
-            System.out.print(numb + "  ");
-        }
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+
     }
 
 }
---
+++ src/main/java/com/jacob/sort/SelectSort.java
@@ -0,0 +1,65 @@
+package com.jacob.sort;
+
+import java.util.Arrays;
+
+public class SelectSort {
+
+    public static void main(String[] args) {
+
+//        int[] numb = {4,5,3,6,2,5,1};
+        int[] numb = new int[80000];
+
+        for (int i = 0; i < 80000; i++) {
+            numb[i] = (int) (Math.random() * 10000000);
+        }
+        long start = System.currentTimeMillis();
+        int minIndex = 0;
+        //先假设最小的是第0个，让它去打擂台
+        int min = numb[0];
+//        System.out.println(Arrays.toString(numb));
+//        //打擂台,第一轮
+//        for (int i = 0; i < numb.length - 1; i++) {
+//            if (numb[i + 1] < min){
+//                minIndex = i + 1;
+//                min = numb[i + 1];
+//            }
+//        }
+//        numb[minIndex] = numb[0];
+//        numb[0] = min;
+//        System.out.println(Arrays.toString(numb));
+//        //打擂台,第二轮
+//        minIndex = 1;
+//        min = numb[1];
+//        for (int i = 0; i < numb.length - 2; i++) {
+//            if (numb[i + 2] < min){
+//                minIndex = i + 2;
+//                min = numb[i + 2];
+//            }
+//        }
+//        numb[minIndex] = numb[1];
+//        numb[1] = min;
+
+        for (int j = 0; j < numb.length - 1; j++){
+            minIndex = j;
+            min = numb[j];
+            for (int i = 1; i < numb.length - j; i++) {
+                if (numb[i + j] < min){
+                    minIndex = i + j;
+                    min = numb[i + j];
+                }
+            }
+            if (minIndex != j){
+                numb[minIndex] = numb[j];
+                numb[j] = min;
+            }
+
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+//        System.out.println(Arrays.toString(numb));
+
+
+    }
+
+}
